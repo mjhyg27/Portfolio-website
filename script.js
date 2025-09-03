6cc54615886b4b038c0dcfbb4fb30f114e76e347
mjhgy27 <miguel_romo02@hotmail.com>
2025-09-03 14:08:09 -0700

    Animations and carousel added, but still needs work

---
+++ script.js
@@ -0,0 +1,85 @@
+
+
+
+let carousels = document.querySelectorAll('.carousel');
+
+
+carousels.forEach( carousel => {
+
+    let slides = carousel.querySelectorAll('li');
+    let list = carousel.querySelector('ul')
+    let slideWidth = 1000; // auto-read width
+    let position = 0;
+    
+    carousel.querySelector('.prev').onclick = function() {
+      position += slideWidth;
+      position = Math.min(position, 0);
+      list.style.transform = `translateX(${position}px)`;
+    };
+    
+    carousel.querySelector('.next').onclick = function() {
+      position -= slideWidth;
+      position = Math.max(position, -slideWidth * (slides.length - 1));
+      list.style.transform = `translateX(${position}px)`;
+    };
+
+});
+
+
+let optionsExperience = {
+    root: null,
+    rootMargin: '0px',
+    scrollMargin: '0px',
+    threshold: [0.3, 0.7, 1]
+}
+
+let experienceObserver = new IntersectionObserver(experienceAnimation, optionsExperience);
+
+function experienceAnimation(entries, observer)  {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animatedJob');
+        } else {
+            entry.target.classList.remove('animatedJob');
+        };
+    });
+
+};
+
+let jobCollection = document.querySelectorAll('.job');
+
+jobCollection.forEach(job => {
+    experienceObserver.observe(job);
+    
+});
+
+let optionsProjects = {
+    root: null,
+    rootMargin: '0px',
+    scrollMargin: '0px',
+    threshold: [0.3, 0.7, 1]
+}
+
+let projectsObserver = new IntersectionObserver(projectsAnimation, optionsProjects);
+
+function projectsAnimation(entries, observer)  {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1;  
+        } else {
+            entry.target.style.opacity = 0;   
+        };
+    });
+
+};
+
+// let projectsCollection = document.querySelectorAll('.job');
+let projectsCollection = document.getElementById('projects').children;
+
+for (let project of projectsCollection) {
+    projectsObserver.observe(project);
+}
+    
+
